4d365c8d089dceb5c770e1c93e0848e73dd14c5c
Paulo Henrique <paulohhartwig@gmail.com>
2018-04-25 22:07:25 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/local/finmap/api/FinmapApplication.java
@@ -0,0 +1,12 @@
+package local.finmap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmapApplication.class, args);
+	}
+}
---
+++ src/test/java/local/finmap/api/FinmapApplicationTests.java
@@ -0,0 +1,16 @@
+package local.finmap.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinmapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
