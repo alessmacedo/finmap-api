7e0dd81354ae12a80b605f1a301ca589843cf48d
Paulo Henrique <paulohhartwig@gmail.com>
2018-04-26 21:30:07 -0300

    Implementação da classe Usuario no pacote Entities

--- src/main/java/local/finmap/api/entities/Usuario.java
+++ src/main/java/local/finmap/api/entities/Usuario.java
@@ -1,7 +1,128 @@
 package local.finmap.api.entities;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 	
+	private int id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	private Date dataNasc;
+	private List<Lancamento> lancamentos;
+	private List<Meta> metas;
+	private List<Conta> contas;
 	
+	public Usuario() {
+		
+	}
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name = "nome", nullable = false)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Column(name = "sobrenome", nullable = false)
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	@Column(name = "email", nullable = false)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Column(name = "senha", nullable = false)
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	@Column(name = "dataNasc", nullable = false)
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	@OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+
+	@OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Meta> getMetas() {
+		return metas;
+	}
+
+	public void setMetas(List<Meta> metas) {
+		this.metas = metas;
+	}
+
+	@OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
+	
+	public String toString() {
+		return "Usuario ["
+				+ "id=" + id
+				+ ", nome=" + nome 
+				+ ", sobrenome=" + sobrenome
+				+ ", email=" + email
+				+ ", senha=" + senha
+				+ ", dataNasc=" + dataNasc
+				+ "]";
+		
+	}
 
 }
