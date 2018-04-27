54c5b50d6ae4564d5c2cee4964488b7300a28333
Paulo Henrique <paulohhartwig@gmail.com>
2018-04-26 23:26:47 -0300

    Implementação da classe Meta no pacote Entities

--- src/main/java/local/finmap/api/entities/Meta.java
+++ src/main/java/local/finmap/api/entities/Meta.java
@@ -1,5 +1,113 @@
 package local.finmap.api.entities;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "meta")
 public class Meta {
 
+	private int id;
+	private String titulo;
+	private String descricao;
+	private Date dataInicio;
+	private Date dataFim;
+	private Double valor;
+	private Usuario usuario;
+	private List<Lancamento> lancamentos;
+	
+	private Meta() {
+		
+	}
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "titulo", nullable = false)
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	@Column(name = "descricao", nullable = false)
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Column(name = "dataInicio", nullable = false)
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	@Column(name = "dataFim", nullable = false)
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	@Column(name = "valor", nullable = false)
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@ManyToOne
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@OneToMany(mappedBy = "meta", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+	
+	public String toString() {
+		return "Meta [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao 
+				+ ", dataInicio=" + dataInicio + ", dataFim=" + dataFim + ", valor=" + valor 
+				+ ", usuario=" + usuario + "]";
+	}
+	
 }
